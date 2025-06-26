9cb5905f6b2b1ce10f89e1c57871dbc6a02f2a0c
Simon Münker <muenker@uni-trier.de>
2025-06-26 13:07:15 +0200

    add autodoc features

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,9 @@
+import sys
+from pathlib import Path
+
+
+sys.path.insert(0, str(Path('../../..', 'src').resolve()))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -6,22 +12,21 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'TWON-LSS'
-copyright = '2025, Simon Münker'
-author = 'Simon Münker'
+project = "TWON-LSS"
+copyright = "2025, Simon Münker"
+author = "Simon Münker"
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = ["sphinx.ext.autodoc", "sphinx.ext.autosummary"]
+autosummary_generate = True
 
-templates_path = ['_templates']
+templates_path = ["_templates"]
 exclude_patterns = []
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
-html_static_path = ['_static']
+html_theme = "alabaster"
+html_static_path = ["_static"]
