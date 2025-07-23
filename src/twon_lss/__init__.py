1a4a6f4c27072e76b79f4b23f0d4e7d57875c3a1
Simon Münker <muenker@uni-trier.de>
2025-07-23 14:33:58 +0200

    switch from relative to absolute import

--- src/twon_lss/__init__.py
+++ src/twon_lss/__init__.py
@@ -1,7 +1,6 @@
-from .simulation import Simulation
-
-from . import ranking
-from . import schemas
-from . import utility
+from twon_lss.simulation import Simulation
+from twon_lss import ranking
+from twon_lss import schemas
+from twon_lss import utility
 
 __all__ = ["Simulation", "ranking", "schemas", "utility"]
