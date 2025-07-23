f725c2d85b445e71cec236142882cede556b96f6
Simon Münker <muenker@uni-trier.de>
2025-07-23 15:11:46 +0200

    decouple agent from user

--- src/twon_lss/schemas/user.py
+++ src/twon_lss/schemas/user.py
@@ -1,29 +1,23 @@
 import pydantic
 
-from twon_lss.agent import AgentInterface
-
 
 class User(pydantic.BaseModel):
     """
     Represents a user in the simulation.
 
-    The `User` class models individual users within the social media simulation,
-    each with a unique identifier and an agent that controls their behavior.
+    The `User` class models individual users within the social media simulation.
 
     Attributes:
         id (int | str): A unique identifier of the user as either a string or integer.
-        agent (AgentInterface): An `AgentInterface` object that controls the user's behavior in the simulation.
 
     Example:
         >>> from src.twon_lss.schemas import User
         ... user = User(
         ...     id="U001",
-        ...     agent=None
         ... )
     """
 
     id: int | str
-    agent: AgentInterface | None = None
 
     def __hash__(self):
         return hash(self.id)
