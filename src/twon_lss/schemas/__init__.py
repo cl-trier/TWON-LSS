1c3025954ba0cc6c96e9dc753159ea2527218375
Simon Münker <muenker@uni-trier.de>
2025-07-23 15:12:04 +0200

    unify interaction and post

--- src/twon_lss/schemas/__init__.py
+++ src/twon_lss/schemas/__init__.py
@@ -1,8 +1,7 @@
 from twon_lss.schemas.user import User
-from twon_lss.schemas.interaction import Interaction, InteractionTypes
-from twon_lss.schemas.post import Post
+from twon_lss.schemas.post import Post, Interaction, InteractionTypes
 from twon_lss.schemas.feed import Feed
 from twon_lss.schemas.network import Network
 
 
-__all__ = ["User", "Interaction", "InteractionTypes", "Post", "Feed", "Network"]
+__all__ = ["User", "Post", "Interaction", "InteractionTypes", "Feed", "Network"]
