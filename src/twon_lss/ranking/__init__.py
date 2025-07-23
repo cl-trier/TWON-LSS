c0513863142bc3b324498b70294f0f26fcaa1f06
Simon Münker <muenker@uni-trier.de>
2025-07-23 14:43:14 +0200

    switch from relative to absolute import

--- src/twon_lss/ranking/__init__.py
+++ src/twon_lss/ranking/__init__.py
@@ -1,8 +1,8 @@
-from ._interface import RankingInterface, RankingArgsInterface
-from . import twon_ranker
+from twon_lss.ranking import twon_ranker
+from twon_lss.ranking._interface import RankingInterface, RankingArgsInterface
 
 __all__ = [
+    "twon_ranker",
     "RankingInterface",
     "RankingArgsInterface",
-    "twon_ranker",
 ]
