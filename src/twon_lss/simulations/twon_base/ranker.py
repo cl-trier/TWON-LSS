a772450aab8ff2a286334ad640600799b8d1f7da
nsschw <s2nsschw@uni-trier.de>
2025-08-25 11:56:16 +0000

    add noise to rankers & add personalized user like ranker

--- src/twon_lss/simulations/twon_base/ranker.py
+++ src/twon_lss/simulations/twon_base/ranker.py
@@ -40,36 +40,30 @@ class Ranker(RankerInterface):
         
 
 class RandomRanker(RankerInterface):
-    llm: LLM
-
     args: RankerArgs = RankerArgs()
 
     def _compute_network(self, post: Post) -> float:
-        return random.uniform(0, 1)
+        return 0.0
 
     def _compute_individual(self, user: User, post: Post, feed: Feed) -> float:   
         return random.uniform(0, 1)
     
 
 class LikeRanker(RankerInterface):
-    llm: LLM
-
     args: RankerArgs = RankerArgs()
 
     def _compute_network(self, post: Post) -> float:
-        return len(post.likes)
+        return len(post.likes) + random.uniform(0, 1) # to prevent "chronological reading"
 
     def _compute_individual(self, user: User, post: Post, feed: Feed) -> float:   
         return 0.0
     
 
 class UserLikeRanker(RankerInterface):
-    llm: LLM
-
     args: RankerArgs = RankerArgs()
 
     def _compute_network(self, post: Post, feed: Feed) -> float:
-        return feed.get_like_count_by_user(post.user)
+        return feed.get_like_count_by_user(post.user) + random.uniform(0, 1) # to prevent "chronological reading"
 
     def _compute_individual(self, user: User, post: Post, feed: Feed) -> float:   
         return 0.0
@@ -98,4 +92,40 @@ class UserLikeRanker(RankerInterface):
                 final_scores[(user, post)] = self.args.noise() * combined_score
 
         return final_scores
+    
+
+class PersonalizedUserLikeRanker(RankerInterface):
+    args: RankerArgs = RankerArgs()
+
+    def _compute_network(self, post: Post, feed: Feed) -> float:
+        return 0.0
+
+    def _compute_individual(self, user: User, post: Post, feed: Feed) -> float:   
+        return feed.get_likes_given_to_user(user, post.user) + random.uniform(0, 1) # Add noise in case that there are no likes, liked user has multiple unread tweets, multiple users have same like count etc.
+
+    def __call__(
+        self, users: typing.List[User], feed: Feed, network: Network
+    ) -> typing.Dict[typing.Tuple[User, Post], float]:
+        logging.debug(f"{len(feed)=}")
+
+        global_scores: typing.Dict[str, float] = {}
+        for post in feed:
+            global_scores[post.id] = self._compute_network(post, feed)
+
+        # retrieve indivual score for visible (if is neighbor) post for each user
+        final_scores: typing.Dict[typing.Tuple[User, Post], float] = {}
+        for user in users:
+            for post in self.get_individual_posts(user, feed, network):
+                individual_score = self._compute_individual(user, post, feed)
+                global_score = global_scores[post.id]
+
+                combined_score = (
+                    self.args.weights.individual * individual_score
+                    + self.args.weights.network * global_score
+                )
+
+                final_scores[(user, post)] = self.args.noise() * combined_score
+
+        return final_scores
+    
     
