d65808d306d51ecaf3ff7e205bd3ae328fb40c1b
nsschw <schwager@uni-trier.de>
2025-11-17 15:11:03 +0000

    feat: include wp3_simulation in module exports

--- src/twon_lss/simulations/__init__.py
+++ src/twon_lss/simulations/__init__.py
@@ -1,4 +1,5 @@
 from twon_lss.simulations import bcm
 from twon_lss.simulations import twon_base
+from twon_lss.simulations import wp3_simulation
 
-__all__ = ["bcm", "twon_base"]
+__all__ = ["bcm", "twon_base", "wp3_simulation"]
